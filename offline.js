6a76e382fa58999213601efdab7b3f0dc98bc34d
istrauch2002 <150802160+istrauch2002@users.noreply.github.com>
2024-12-26 09:28:42 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1735186652,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/Спрайт2-sheet0.png",
+		"images/Спрайт3-sheet0.png",
+		"images/Спрайт4-sheet0.png",
+		"images/Спрайт5-sheet0.png",
+		"images/Спрайт8-sheet0.png",
+		"images/Спрайт9-sheet0.png",
+		"images/Спрайт10-sheet0.png",
+		"images/Спрайт11-sheet0.png",
+		"images/Спрайт-sheet0.png",
+		"images/Спрайт12-sheet0.png",
+		"images/Спрайт13-sheet0.png",
+		"images/Спрайт14-sheet0.png",
+		"images/Спрайт15-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"channel.html",
+		"cordova.oauth2.js"
+	]
+}
